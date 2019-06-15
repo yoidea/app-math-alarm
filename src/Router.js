11eaea9ffa60bec0aeea8c2794de168a666ad6f8
Yoidea <yoidea.cc@gmail.com>
2019-06-15 20:22:50 +0900

    :sparkles: Create a page and add routing

--- src/Router.js
+++ src/Router.js
@@ -1,13 +1,14 @@
 import React, { Component } from "react";
 import { HashRouter, Route } from "react-router-dom";
 import Home from "./pages/Home";
+import Ring from "./pages/Ring";
 
 class Router extends Component {
   render() {
     return (
       <HashRouter>
         <Route exact path="/" component={Home} />
-        <Route exact path="/test" component={Home} />
+        <Route exact path="/ring" component={Ring} />
         <Route exact path="/params/:id(\d+)" component={Home} />
       </HashRouter>
     );
--- src/pages/Ring.js
+++ src/pages/Ring.js
@@ -1,21 +1,17 @@
 import React, { Component } from "react";
-import { Link } from "react-router-dom";
 
 class Home extends Component {
   render() {
     return (
       <div
-        className="Home blue-grey darken-1"
+        className="Home teal darken-1"
         style={{
           margin: "0",
           padding: "3em",
           minHeight: "100vh"
         }}
       >
-        <h1 className="white-text center">Hello from Materialize</h1>
-        <Link className="btn btn-large" to="ring">
-          Ring
-        </Link>
+        <h1 className="white-text center">Ringing!</h1>
       </div>
     );
   }
